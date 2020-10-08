71c8359c60bcf3d964ae7e5884d85c718be20720
MSK17A <legendary512@gmail.com>
2020-10-09 01:40:11 +0300

    .

---
+++ Data_Structures/Graphs/Graph.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#define MAXV    1000 /* Maximum number of vertices */
+
+class Egdenode
+{
+    int y; /* Adjency info */
+    int weight; /* edge weight */
+    Egdenode* next; /* Next edge in list */
+};
+
+class Graph
+{
+private:
+    Egdenode* edges[MAXV+1]; /* adjacency info */
+    int degree[MAXV+1]; /* outdegree if each vertex */
+    int nVertices; /* number of vertices */
+    int nEdges; /* number of edges */
+    bool directed; /* is the graph directed? */
+
+public:
+    Graph(/* args */);
+    ~Graph();
+};
+
+Graph::Graph(/* args */)
+{
+}
+
+Graph::~Graph()
+{
+}
