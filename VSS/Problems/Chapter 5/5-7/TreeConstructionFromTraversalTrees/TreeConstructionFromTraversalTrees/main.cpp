ce0609653868b6c55d74bb1f0f587b95c05e2a13
Mohammed Sammak <legendary512@gmail.com>
2020-12-01 11:43:57 +0300

    .

--- VSS/Problems/Chapter 5/5-7/TreeConstructionFromTraversalTrees/TreeConstructionFromTraversalTrees/main.cpp
+++ VSS/Problems/Chapter 5/5-7/TreeConstructionFromTraversalTrees/TreeConstructionFromTraversalTrees/main.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
+#include"BinarySearchTrees.h"
+
+using namespace std;
 
 int main()
 {
+	tree* Tree;
+	Tree = new tree();
+
+	int discovered[6];
+	int pre_order[6];
+	int in_order[6];
+
+
 
 	return 0;
 }
